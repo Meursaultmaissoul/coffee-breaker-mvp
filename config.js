0738bfb91eee80abfd96f18ac871237f104948cf
Meursaultmaissoul <2002kotaro.h@gmail.com>
2025-08-22 09:39:24 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
+// Use your GAS Web App /exec URL
 window.CONFIG = {
-    mode: 'gas', // 'gas' now, switch to 'auto' later
-    gas: {
-        url: 'https://script.google.com/macros/s/AKfycbypx_biZEjw222Szshr5CwoKmvwZvTkYep4Ha2yzi8bQf_P3XtWBJeTbdcm7gQyHSJt/exec' // <- your Web App URL
-    },
-    auto: {
-        // (Later) Firebase web config + hostingBaseUrl
-    }
+  mode: 'gas',
+  gas: { url: 'https://script.google.com/macros/s/AKfycbypx_biZEjw222Szshr5CwoKmvwZvTkYep4Ha2yzi8bQf_P3XtWBJeTbdcm7gQyHSJt/exec' }
 };
-window.API = (window.CONFIG.mode === 'gas') ? window.API_GAS : window.API_AUTO;
+window.API = window.API_GAS;
